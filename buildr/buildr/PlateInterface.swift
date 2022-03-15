6aa1018b2fcfdf8b673ecf3fa48c22e2a28852d4
eliyazdi <eli.s.yazdi@gmail.com>
2022-03-15 16:31:18 -0400

    Worked on BLE

--- buildr/buildr/PlateInterface.swift
+++ buildr/buildr/PlateInterface.swift
@@ -8,15 +8,7 @@
 import Foundation
 import CoreBluetooth
 
-final class PlateInterface: CBPeripheralDelegate, CBCentralManagerDelegate{
-    private var centralManager: CBCentralManager!
-    private var peripheral: CBPeripheral!
-    
-    static let shared = PlateInterface()
-    
-    private init(){
-        centralManager = CBCentralManager(delegate: self, queue: nil)
-    }
+final class PlateInterface{
     
     
 }
--- buildr/buildr/ViewController.swift
+++ buildr/buildr/ViewController.swift
@@ -6,14 +6,49 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController {
+    
+    var centralManager: CBCentralManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
 
 }
 
+extension ViewController :  CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+           central.scanForPeripherals(withServices: nil, options: nil)
+           print("Scanning...")
+        }
+    }
+        
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+      guard peripheral.name != nil else {return}
+      
+      if peripheral.name! == "Sensor Plate" {
+      
+        print("Sensor Found!")
+        //stopScan
+        cbCentralManager.stopScan()
+        
+        //connect
+        cbCentralManager.connect(peripheral, options: nil)
+        self.peripheral = peripheral
+       
+            }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+      //discover all service
+      
+      peripheral.discoverServices(nil)
+      peripheral.delegate = self
+
+    }
+}
